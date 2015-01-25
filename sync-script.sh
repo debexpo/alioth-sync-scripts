e27d702881304bdaf9ba012ee9ae96388fa58da7
Asheesh Laroia <asheesh@asheesh.org>
2015-01-24 23:00:42 -0500

    Add a version 0 sync script

---
+++ sync-script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+# Strategy:
+#
+# For JUST the master branch, if it gets an update on github,
+# push that to Alioth.
+#
+# If that fails for some reason, this thing will send output
+# to stdout, which will cause the cron user to get emailed.
+# To avoid this happening repeatedly, this script chmod itself
+# not executable.
+#
+# TODO: create a status images.
+
+cd ~/debexpo  # This is where the code is.
+git fetch -q origin  # This should fetch from github.
+git push --quiet alioth HEAD:master  # We attempt to push.
+
+# If that didn't work, then we let the shell exit 1,
+# and print an error.
